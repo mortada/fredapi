d3340d5842c6dd26da0d304bfebeb186917075c6
Mortada Mehyar <mortada.mehyar@gmail.com>
2013-09-07 16:20:48 -0700

    added api implementation

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-fredapi
-=======
+# fredapi: Python API for FRED (Federal Reserve Economic Data)
+
+## What is it
+
+`fredapi` is a Python API for the [FRED](http://research.stlouisfed.org/fred2/) data provided by the
+Federal Reserve Bank of St. Louis. `fredapi` provides a wrapper in python to the 
+[FRED web service](http://api.stlouisfed.org/docs/fred/), and also provides several conveninent methods
+for parsing and analyzing point-in-time data (i.e. historic data revisions) from [ALFRED](http://research.stlouisfed.org/tips/alfred/)
+
+`fredapi` makes use of `pandas` and returns data to you in a `pandas` `Series` or `DataFrame`
+
+## Installation
+
+```sh
+pip install fredapi
+```
+or
+```sh
+conda install fredapi
+```
+
+## Basic Usage
+
+First you need an API key, you can [apply for one](http://api.stlouisfed.org/api_key.html) for free on the FRED website.
+Once you have your API key, you can set it in one of three ways:
+
+* set it to the evironment variable FRED_API_KEY
+* save it to a file and use the 'api_key_file' parameter
+* pass it directly as the 'api_key' parameter
+
+```python
+from fredapi import Fred
+fred = Fred(api_key='insert api key here')
+data = fred.get_series('SP500')
+```
+
+## Working with data revisions
+Many economic data series contain frequent revisions. `fredapi` provides several convenient methods for handling data revisions and answering the quesion of what-data-was-known-when.
+
+In [ALFRED](http://research.stlouisfed.org/tips/alfred/) there is the concept of a *vintage* date. Basically every *observation* can have three dates associated with it: *date*, *realtime_start* and *realtime_end*. 
+
+- date: the date the value is for
+- realtime_start: the first date the value is valid
+- realitime_end: the last date the value is valid
+
+For instance, there has been three observations (data points) for the GDP of 2014 Q1:
+
+```xml
+<observation realtime_start="2014-04-30" realtime_end="2014-05-28" date="2014-01-01" value="17149.6"/>
+<observation realtime_start="2014-05-29" realtime_end="2014-06-24" date="2014-01-01" value="17101.3"/>
+<observation realtime_start="2014-06-25" realtime_end="2014-07-29" date="2014-01-01" value="17016.0"/>
+```
+
+This means the GDP value for Q1 2014 has been released three times. First release was on 4/30/2014 for a value of 17149.6, and then there have been two revisions on 5/29/2014 and 6/25/2014 for revised values of 17101.3 and 17016.0, respectively.
+
+### Get first data release only (i.e. ignore revisions)
+
+### Get latest data
+
+### Get latest data known on a given date
+
+### Get all data release dates
+
+## Dependencies
+- [pandas](http://pandas.pydata.org/)
+- [python-dateutil](http://labix.org/python-dateutil)
---
+++ fredapi/fred.py
@@ -0,0 +1,127 @@
+import os
+import sys
+import xml.etree.ElementTree as ET
+import urllib2
+from dateutil.parser import parse
+import pandas as pd
+
+
+class Fred(object):
+    earliest_realtime_start = '1776-07-04'
+    latest_realtime_end = '9999-12-31'
+    nan_char = '.'
+
+    def __init__(self,
+                 api_key=None,
+                 api_key_file=None):
+        self.api_key = None
+        if api_key is not None:
+            self.api_key = api_key
+        elif api_key_file is not None:
+            f = open(api_key_file, 'r')
+            self.api_key = f.readline().strip()
+            f.close()
+        else:
+            self.api_key = os.environ.get('FRED_API_KEY')
+
+        if self.api_key is None:
+            raise ValueError("You need to set a valid API key. You can set it in 3 ways: pass the string with api_key, "
+                             "or set api_key_file to a file with the api key in the first line, or set the environment "
+                             "variable 'FRED_API_KEY' to the value of your api key. You can sign up for a free api key "
+                             "on the Fred website at http://research.stlouisfed.org/fred2/")
+
+    def __fetch_data(self, url):
+        response = urllib2.urlopen(url)
+        root = ET.fromstring(response.read())
+        return root
+
+    def get_series_info(self, series_id):
+        url = "http://api.stlouisfed.org/fred/series?series_id=%s&api_key=%s" % (series_id, self.api_key)
+        root = self.__fetch_data(url)
+        return root.getchildren()[0].attrib
+    
+    def get_series(self, series_id):
+        url = "http://api.stlouisfed.org/fred/series/observations?series_id=%s&api_key=%s" % (series_id, self.api_key)
+        root = self.__fetch_data(url)
+        data = {}
+        for child in root.getchildren():
+            val = child.get('value')
+            if val == self.nan_char:
+                val = float('NaN')
+            else:
+                val = float(val)
+            data[parse(child.get('date'))] = val
+        return pd.Series(data)
+    
+    def get_series_latest_release(self, series_id):
+        return self.get_series(series_id)
+
+    def get_series_first_release(self, series_id):
+        df = self.get_series_all_releases(series_id)
+        first_release = df.groupby('date').head(1)
+        return first_release.set_index('date')['value']
+
+    def get_series_as_of_date(self, series_id, as_of_date):
+        as_of_date = pd.to_datetime(as_of_date)
+        df = self.get_series_all_releases(series_id)
+        return df[df['realtime_start'] <= as_of_date]
+
+    def get_series_all_releases(self, series_id):
+        url = "http://api.stlouisfed.org/fred/series/observations?series_id=%s&api_key=%s&realtime_start=%s&realtime_end=%s" % (series_id,
+                                                                                                                                self.api_key,
+                                                                                                                                self.earliest_realtime_start,
+                                                                                                                                self.latest_realtime_end)
+        root = self.__fetch_data(url)
+        data = {}
+        i = 0
+        for child in root.getchildren():
+            val = child.get('value')
+            if val == self.nan_char:
+                val = float('NaN')
+            else:
+                val = float(val)
+            realtime_start = parse(child.get('realtime_start'))
+            # realtime_end = parse(child.get('realtime_end'))
+            date = parse(child.get('date'))
+            
+            data[i] = {'realtime_start': realtime_start,
+                       # 'realtime_end': realtime_end,
+                       'date': date,
+                       'value': val}
+            i += 1
+        data = pd.DataFrame(data).T
+        return data
+
+    def get_series_vintage_dates(self, series_id):
+        url = "http://api.stlouisfed.org/fred/series/vintagedates?series_id=%s&api_key=%s" % (series_id, self.api_key)
+        root = self.__fetch_data(url)
+        dates = []
+        for child in root.getchildren():
+            dates.append(parse(child.text))
+        return dates
+
+    def search(self, text, fulltext_search=True, limit=100):
+        import urllib
+        url = "http://api.stlouisfed.org/fred/series/search?search_text=%s&api_key=%s" % (urllib.quote_plus(text), self.api_key)
+        root = self.__fetch_data(url)
+
+        series_ids = []
+        data = {}
+
+        for child in root.getchildren():
+            series_id = child.get('id')
+            series_ids.append(series_id)
+            data[series_id] = {"id": series_id}
+            fields = ["realtime_start", "realtime_end", "title", "observation_start", "observation_end",
+                      "frequency", "frequency_short", "units", "units_short", "seasonal_adjustment",
+                      "seasonal_adjustment_short", "last_updated", "popularity", "notes"]
+            for field in fields:
+                data[series_id][field] = child.get(field)
+
+        data = pd.DataFrame(data, columns=series_ids).T
+        # parse datetime columns
+        for field in ["realtime_start", "realtime_end", "observation_start", "observation_end", "last_updated"]:
+            data[field] = data[field].apply(parse)
+        # set index name
+        data.index.name = 'series id'
+        return data
