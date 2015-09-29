1d3cd9499b45e421de980e112696bd1a7d7f6744
Mortada Mehyar <mortada.mehyar@gmail.com>
2015-09-29 08:54:25 -0700

    minor cleanup

--- fredapi/tests/__init__.py
+++ fredapi/tests/__init__.py
@@ -1,4 +1,1 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-# This file is needed by Python 2.7 to figure that tests is a package.
+""" this file is needed for python to consider this directory a module """
