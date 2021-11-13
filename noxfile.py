22189e6e39d23b4bcf8870838d8195a58e78fbd5
gw-moore <gwmoore92@gmail.com>
2021-11-12 22:13:15 -0500

    Add linting to nox

--- noxfile.py
+++ noxfile.py
@@ -6,3 +6,11 @@ def docs(session):
     """Rebuild and deploy sphinx documentation to gh-pages branch"""
     session.install(".[docs]")
     session.run(["make", "gh-deploy"])
+
+
+@nox.session
+def lint(session):
+    """Apply black formatting"""
+    session.install("black")
+    session.run("black", "fredapi")
+    session.run("black", "tests")
