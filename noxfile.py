6bcde3012445eeb9199757fdd60066b8ab74a046
gw-moore <gwmoore92@gmail.com>
2021-11-13 10:43:48 -0500

    Create separate nox session for interrogate

--- noxfile.py
+++ noxfile.py
@@ -5,9 +5,15 @@ import nox
 def docs(session):
     """Rebuild and deploy sphinx documentation to gh-pages branch"""
     session.install(".[docs]")
-    # Run interrogate to create fresh interrorgate badge for docs
+    session.cd("docs")
+    session.run("make", "gh-deploy")
+
+
+@nox.session
+def interrorgate(session):
+    """Run interrogate and create fresh interrogate badge for README"""
+    session.install(".[lint]")
     session.run("interrogate", "--generate-badge", "docs/_static/", "fredapi")
-    session.run("docs/make", "gh-deploy")
 
 
 @nox.session
@@ -18,7 +24,7 @@ def lint(session):
     * Run isort
     * Run flake8
     """
-    session.install("black", "isort", "flake8")
+    session.install(".[lint]")
     session.run("black", "fredapi")
     session.run("isort", "fredapi")
     session.run("flake8", "fredapi")
@@ -32,7 +38,7 @@ def lint_tests(session):
     * Run isort
     * Run flake8
     """
-    session.install("black", "isort", "flake8")
+    session.install(".[lint]")
     session.run("black", "tests")
     session.run("isort", "tests")
     session.run("flake8", "tests")
